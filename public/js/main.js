2ef3959a5b53dcf46f7134217108afad73f77971
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-05-07 13:56:01 -0500

    Updated main.handlebars -- Not succeeding in getting the login page.

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+// document.getElementById("home").addEventListener("click", displayDate);
+// document.getElementById("libraries").addEventListener("click", displayDate);
+// document.getElementById("search").addEventListener("click", displayDate);
+// document.getElementById("dashboard").addEventListener("click", displayDate);
+// document.getElementById("login").addEventListener("click", displayDate);
