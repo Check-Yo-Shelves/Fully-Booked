375797172355a1b1978d3258de0e741d5c404ca4
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-05-06 21:40:45 -0500

    Added library route to index and started libraryRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const libraryRoutes = require('./libraryRoutes');
 const userRoutes = require('./userRoutes');
 
 router.use('/users', userRoutes);
 // library route
+router.use('/library', libraryRoutes);
 // librarybook route
 // book routes
 
--- controllers/api/libraryRoutes.js
+++ controllers/api/libraryRoutes.js
@@ -1,4 +1,43 @@
-const router = require('express').Router();
-const { Library, LibraryBook, Book } = require('../../models');
+const router = require("express").Router();
+const { Library, LibraryBook, Book, User } = require("../../models");
+
+// Get all libaries (GET)
+router.get("/", async (req, res) => {
+  try {
+      const libraryData = await Library.findAll();
+      res.status(200).json(libraryData);
+    } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get libraries by ZIP (GET)
+router.get("/zip", async (req, res) => {
+  try {
+    const libraryData = await Library.findAll({
+      where: {
+        zip_code: req.body.zip_code,
+      },
+    });
+    // Error isn't thrown. -- ZIP code that doesn't match any libraries returns an empty array rather than an error.
+    if (!libraryData) {
+      res
+        .status(400)
+        .json({
+          message: "No libraries found matching that ZIP. Please try again.",
+        });
+      return;
+    }
+    res.status(200).json(libraryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Add a new library (POST)
+
+// Update an owned libary (PUT)
+
+// Delete an owned libary (DELETE)
 
 module.exports = router;
