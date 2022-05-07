bdb063e961c564263d63550686f7e6d91bd7a5ca
Miguel Escobar <mae2136@gmail.com>
2022-05-06 21:26:41 -0500

    - Update LibraryBook routes, and minor cosmetic handlebar changes.

--- controllers/api/libraryBookRoutes.js
+++ controllers/api/libraryBookRoutes.js
@@ -1,4 +1,57 @@
 const router = require('express').Router();
-const { Library, LibraryBook, Book } = require('../../models');
+const { LibraryBook, User } = require('../../models');
+
+// This route allows us to GET all libraryBooks
+router.get('/', async (req, res) => {
+    try {
+        const libraryBookData = await LibraryBook.findAll({
+            include: [{ model: User }],
+        });
+
+        res.status(200).json(libraryBookData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// This route allows us to GET libraryBooks by id
+router.get('/:id', async (req, res) => {
+    try {
+        const libraryBookData = await LibraryBook.findByPk(req.params.id, {
+            include: [{ model: User }],
+        });
+
+        if (!libraryBookData) {
+            res.status(404).json({ message: `No library book found with that id!` });
+            return;
+          }
+
+        res.status(200).json(libraryBookData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// This route allows both checking libraryBook both in and out.
+router.put('/:id', async (req, res) => {
+    try {
+        // Update the libraryBook that is referenced by id with req.body parameters (which are user_id & checked_out property)
+        // Logic for this will go in js folder.
+        const libraryBookData = await LibraryBook.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!libraryBookData) {
+            res.status(404).json({ message: `No library book found with that id!`});
+            return;
+        }
+
+        res.status(200).json(libraryBookData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
