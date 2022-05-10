acbec9beb21576681145798835a46af87e17eed3
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-05-10 12:30:12 -0500

    Search console logs user input and gives no errors.

--- public/js/search.js
+++ public/js/search.js
@@ -1,29 +1,44 @@
 // SEARCHING ROUTE
 // drop down to select types of searches
 // search by ISBN
+
 // search by Name if we lucky
 const searchHandler = async (e) => {
   e.preventDefault();
-    console.log("search function started")
-  const search = document.querySelector("#searchBar").value();
+  const searchBar = document.getElementById("searchBar");
+  var search = searchBar.value;
+//   if (search !== Number) {
+//       console.error();
+//       return;
+//   }
 
   console.log(search);
+  console.log("search function started");
 
-  if (search = !null) {
-    const response = await fetch(`/api/books/:id`, {
+  if (search) {
+    const response = await fetch(`/api/books/:${search}`, {
       method: "GET",
-      body: isbn,
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
     });
-  }
-  if (response.ok) {
-      response.status(200).json(response);
-
+    if (response.ok) {
+      console.log(response);
+    }
   } else {
-      alert('No results. Please try your search again.')
+    alert("No results. Please try your search again.");
   }
 };
 
-document.getElementById("searchBtn").addEventListener("submit", searchHandler);
+document.getElementById("searchBtn").addEventListener("click", (e) => {
+  searchHandler(e);
+  console.log("Click Event");
+  e.preventDefault;
+});
+
+// startSearch.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     console.log("Click event");
+//     searchHandler();
+
+// })
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,8 +1,8 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
-    
+    console.log("Signup working")
     // Pull query selectors from Nat's handlebars
-    const name = document.querySelector('#name-signup').value.trim();
+    const name = document.getElementById('name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
     const zip_code = document.querySelector('#zip_code-signup').value.trim();
