603888d71ec58e16a2876a93dd844c4eaeb97a81
Miguel Escobar <mae2136@gmail.com>
2022-05-10 20:37:26 -0500

    Dashboard route now also gets book object.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -62,8 +62,26 @@ router.get('/dashboard', withAuth, async (req, res) => {
     const user = userData.get({ plain: true });
     console.log(user);
 
+    let checkedOut = [];
+
+    for (let i = 0; i < user.librarybooks.length; i++) {
+      checkedOut.push(user.librarybooks[i].book_id);
+    }
+    console.log("Checked out Array", checkedOut);
+
+    const bookData = await Book.findAll({
+      where: {
+        id: checkedOut,
+      },
+    });
+    // console.log(bookData);
+
+    const books = bookData.map((book => book.get({ plain: true })));
+    console.log("Checked out books", books);
+
     res.render('dashboard', {
       user,
+      books,
       logged_in: req.session.logged_in,
     });
     console.log('Dashboard Route OK');
