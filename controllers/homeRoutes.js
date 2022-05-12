cf6fe790f745cb0875c2c0efc59215281a921f9e
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-05-12 00:25:49 -0500

    Added logged_in to bookinfo/:title route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -53,45 +53,45 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-// router.get('/dashboard', withAuth, async (req, res) => {
-//   try {
-//     // Get User's checked owned libraries to display here.
-//     const userData = await User.findByPk(req.session.user_id, {
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Get User's checked owned libraries to display here.
+    const userData = await User.findByPk(req.session.user_id, {
 
-//       include: [{ model: Library }, { model: LibraryBook }],
-//       attributes: { exclude: ['password'] },
-//     });
+      include: [{ model: Library }, { model: LibraryBook }],
+      attributes: { exclude: ['password'] },
+    });
 
-//     const user = userData.get({ plain: true });
-//     console.log(user);
+    const user = userData.get({ plain: true });
+    console.log(user);
 
-//   let checkedOut = [];
+  let checkedOut = [];
 
-//   for (let i = 0; i < user.librarybooks.length; i++) {
-//     checkedOut.push(user.librarybooks[i].book_id);
-//   }
-//   console.log("Checked out Array", checkedOut);
+  for (let i = 0; i < user.librarybooks.length; i++) {
+    checkedOut.push(user.librarybooks[i].book_id);
+  }
+  console.log("Checked out Array", checkedOut);
 
-//   const bookData = await Book.findAll({
-//     where: {
-//       id: checkedOut,
-//     },
-//   });
-//   // console.log(bookData);
+  const bookData = await Book.findAll({
+    where: {
+      id: checkedOut,
+    },
+  });
+  // console.log(bookData);
 
-//   const books = bookData.map((book => book.get({ plain: true })));
-//   console.log("Checked out books", books);
+  const books = bookData.map((book => book.get({ plain: true })));
+  console.log("Checked out books", books);
 
-//   res.render('dashboard', {
-//     user,
-//     books,
-//     logged_in: req.session.logged_in,
-//   });
-//   console.log('Dashboard Route OK');
-// } catch (err) {
-//   res.status(500).json(err);
-//   }
-// });
+  res.render('dashboard', {
+    user,
+    books,
+    logged_in: req.session.logged_in,
+  });
+  console.log('Dashboard Route OK');
+} catch (err) {
+  res.status(500).json(err);
+  }
+});
 
 router.get('/libraryinfo/:id', async (req, res) => {
   try {
@@ -124,7 +124,7 @@ router.get('/bookinfo/:title', async (req, res) => {
     const thing = booksFound;
     const [book] = thing;
     console.log(book.title);
-    res.render('bookinfo', { book });
+    res.render('bookinfo', { book, logged_in: req.session.logged_in, });
   } catch (err) {
     res.status(500).json(err);
   }
