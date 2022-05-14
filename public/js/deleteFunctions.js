a55e1210a028210437fa2db73e624dbc2bdfc9f2
Miguel Escobar <mae2136@gmail.com>
2022-05-13 22:04:46 -0500

    Accidentally Left this function off.

--- public/js/deleteFunctions.js
+++ public/js/deleteFunctions.js
@@ -11,23 +11,3 @@ let deleteLibrary = async (library_id) => {
         alert('Failed to delete library. Please try again later.');
     }
 };
-
-let checkInBook = async (book_id) => {
-    console.log("Check-in Book Button Clicked:", book_id)
-    let checkIn = {
-        checked_out: false,
-        user_id: null,
-    };
-
-    const response = await fetch(`/api/librarybook/${book_id}`, {
-        method: 'PUT',
-        body: JSON.stringify(checkIn),
-        headers: { 'Content-Type': 'application/json' },
-    });
-    console.log(response)
-    if (response.ok) {
-        document.location.replace('/dashboard');
-    } else {
-        alert('Failed to check in the book. Please try again later.');
-    }
-};
